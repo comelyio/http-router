40ded98f0ae7dad4eeeb2e6d74255be898f6611f
M. Furqan Siddiqui <hello@furqansiddiqui.com>
2018-02-10 23:49:58 +0500

    HTTP headers

---
+++ Controller/Data/Headers.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * This file is part of Comely package.
+ * https://github.com/comelyio/comely
+ *
+ * Copyright (c) 2016-2018 Furqan A. Siddiqui <hello@furqansiddiqui.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code or visit following link:
+ * https://github.com/comelyio/comely/blob/master/LICENSE
+ */
+
+declare(strict_types=1);
+
+namespace Comely\IO\HttpRouter\Controller;
+
+use Comely\IO\HttpRouter\Controller\Data\AbstractIterableData;
+
+/**
+ * Class Headers
+ * @package Comely\IO\HttpRouter\Controller
+ */
+class Headers extends AbstractIterableData
+{
+    /**
+     * @param string $key
+     * @param string $value
+     * @return bool
+     */
+    public function set(string $key, string $value): bool
+    {
+        return $this->setProp($key, $value);
+    }
+
+    /**
+     * @param string $key
+     * @return null|string
+     */
+    public function get(string $key): ?string
+    {
+        $prop = $this->getProp($key);
+        return $prop->key ?? null;
+    }
+}
